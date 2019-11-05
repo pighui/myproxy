0fbfeaf2afbdc46eda682598099b64d7369fc3a8
pighui <pighui233@163.com>
2019-11-05 14:13:14 +0800

    新增获取随机代理函数

--- get_proxy.py
+++ get_proxy.py
@@ -7,7 +7,7 @@
 import requests
 
 
-def get_proxy(params: dict = {}):
+def get_proxies(params: dict = {}):
     '''
     :param params: 参数字典 默认为空
     :return: 返回一个包含多条代理信息的列表，列表的每一个元素是一个字典
@@ -21,19 +21,35 @@ def get_proxy(params: dict = {}):
         return None
 
 
+def random_proxy():
+    '''
+    :return: 返回一个代理信息字典
+    '''
+    try:
+        response = requests.get('http://127.0.0.1:8888/ip/random/')
+        if response.status_code == 200:
+            result = response.json()
+            return result
+    except ConnectionError:
+        return None
+
+
 if __name__ == '__main__':
     # 获取一条代理
-    ip1 = get_proxy()
+    ip1 = get_proxies()
     print(ip1)
     # 获取多条代理
-    ip2 = get_proxy({'count': 3})
+    ip2 = get_proxies({'count': 3})
     print(ip2)
     # 获取匿名代理
-    ip3 = get_proxy({'anonymity': 1})
+    ip3 = get_proxies({'anonymity': 1})
     print(ip3)
     # 获取https代理
-    ip4 = get_proxy({'protocol': 'https'})
+    ip4 = get_proxies({'protocol': 'https'})
     print(ip4)
     # 获取多条匿名的https代理
-    ip5 = get_proxy({'count': 3, 'anonymity': 1, 'protocol': 'https'})
+    ip5 = get_proxies({'count': 3, 'anonymity': 1, 'protocol': 'https'})
     print(ip5)
+    # 随机获取一条代理
+    ip6 = random_proxy()
+    print(ip6)
