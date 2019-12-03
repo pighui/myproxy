e6ad846a57b7edfa5a86fbaad14000ad60c6a7e4
xuhui <pighui233@163.com>
2019-12-03 15:03:01 +0800

    修改with上下文逻辑

--- web/dao/__init__.py
+++ web/dao/__init__.py
@@ -9,21 +9,26 @@ class DB():
     def __init__(self):
         self.conn = pymysql.connect(**DB_CONFIG)
         # print('--Connect database OK--')
-
+        self.cursor = None
     def __enter__(self):
         # 检查当前连接是否有效
         self.conn.ping(reconnect=True)
         # 进入上下文时，需要返回一个cursor对象
-        return self.conn.cursor(cursor=DictCursor)
-
+        if self.cursor is None:
+            self.cursor = self.conn.cursor(cursor=DictCursor)
+        return self.cursor
     def __exit__(self, exc_type, exc_val, exc_tb):
         # 判断是否正常的方式退出上下文
-        if not exc_type:
-            # 如果是正常情况，则提交事务
-            self.conn.commit()
-        else:
+        if exc_type:
             # 如果非正常，则回滚事务
             self.conn.rollback()
+        else:
+            # 如果是正常情况，则提交事务
+            self.conn.commit()
+            # 关闭连接
+            self.close()
+        # 告诉解析器自己处理异常
+	    return True
 
     def close(self):
         self.conn.close()
